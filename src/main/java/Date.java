b044141460cc4dd3357535faf4fd5da65d432683
Ashikur-Rahman-Any <64116937+Ashikur-Rahman-Any@users.noreply.github.com>
2024-02-21 01:55:55 +0000

    Initial commit

---
+++ src/main/java/Date.java
@@ -0,0 +1,16 @@
+public class Date {
+  public static void main(String[] args) {
+
+    String day = "Monday";
+    int date = 18;
+    String month = "September";
+    int year = 2021;
+
+    System.out.println("American format:");
+
+    System.out.println("American format:");
+
+    System.out.println(day + ", " + month + " " + date + ", " + year);
+
+  }
+}
